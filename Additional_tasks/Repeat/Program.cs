002f14ad5929595c70dc7a57cfae6f8b3d0a14d5
Dmitriy <maradonna05@yandex.ru>
2022-10-25 14:17:47 +0300

    Finished Repeat Lesson 2

--- Additional_tasks/Repeat/Program.cs
+++ Additional_tasks/Repeat/Program.cs
@@ -105,3 +105,118 @@
 
 
 
+//                  Максимум из 9 данных чисел.
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+// int a1 = 15;
+// int b1 = 22;
+// int c1 = 33;
+// int a2 = 11;
+// int b2 = 67;
+// int c2 = 45;
+// int a3 = 31;
+// int b3 = 1112;
+// int c3 = 65;
+
+// // int max1 = Max(a1, b1, c1);
+// // int max2 = Max(a2, b2, c2);
+// // int max3 = Max(a3, b3, c3);
+// // int max = Max(max1, max2, max3);
+
+// int max = Max(Max(a1, b1, c1),Max(a2, b2, c2),Max(a3, b3, c3));
+
+// Console.WriteLine(max);
+
+
+
+//              Нахождение максимального из 9 чисел(Массив)
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+// int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+
+// array[0] = 12;
+// Console.WriteLine(array[4]);
+
+// int result = Max(
+//     Max(array[0], array[1], array[2]),
+//     Max(array[3], array[4], array[5]),
+//     Max(array[6], array[7], array[8])
+// );
+
+// Console.WriteLine(result);
+
+
+
+//              Поиск позиции искомого элемента
+// int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+// int n = array.Length;
+// int find = 18;
+// int index = 0;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
+
