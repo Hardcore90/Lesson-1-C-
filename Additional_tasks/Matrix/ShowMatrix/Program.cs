2066db021983630d467ddf1bc0d5a722e6402e16
Dmitriy <maradonna05@yandex.ru>
2022-11-02 15:34:11 +0300

    Finished Task 48(ShowMatrix)

---
+++ Additional_tasks/Matrix/ShowMatrix/Program.cs
@@ -0,0 +1,32 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] CreateMatrix(int m, int n, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min,max);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j],2} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+int[,] matrix = CreateMatrix(m, n, 1, 10);
+PrintMatrix(matrix);
